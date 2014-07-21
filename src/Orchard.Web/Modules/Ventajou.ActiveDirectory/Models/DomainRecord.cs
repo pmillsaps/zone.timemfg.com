e33c7af640050a51d03b361c62a21ca5c4506314
PaulL <PaulL@timemfg.com>
2014-07-21 09:13:15 -0500

    Time.OrderLog controller added

---
+++ src/Orchard.Web/Modules/Ventajou.ActiveDirectory/Models/DomainRecord.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ventajou.ActiveDirectory.Models
+{
+	public class DomainRecord
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string UserName { get; set; }
+		public virtual string Password { get; set; }
+	}
+}
