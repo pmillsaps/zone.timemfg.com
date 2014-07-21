91068f6413703e0bd2e096acd26b0d5b4648e516
PaulL <PaulL@timemfg.com>
2014-07-21 10:23:42 -0500

    Added Time.OrderLog/Routes.cs

---
+++ src/Orchard.Web/Modules/Time.OrderLog/Routes.cs
@@ -0,0 +1,39 @@
+﻿using Orchard.Mvc.Routes;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Time.OrderLog
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 5,
+                     Route = new Route(
+                        "OrderLog/{action}/{id}",
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"},
+                                                    {"controller", "OrderLog"},
+                                                    {"action", "Index"},
+                                                    {"id", null}
+                                                },
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"},
+                                                    {"controller", "OrderLog"},
+                                                },
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"}
+                                                },
+                        new MvcRouteHandler())}};
+        }
+    }
+}
