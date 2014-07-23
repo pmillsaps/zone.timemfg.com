ba9f29945c2b853cf8109d8407dd751908069340
Paul Millsaps <paulm@timemfg.com>
2014-07-23 14:01:08 -0500

    Updated the LogOn view to include domain user information

---
+++ src/Orchard.Web/Modules/Time.OrderLog/Permissions.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+using System.Collections.Generic;
+
+namespace Time.OrderLog
+{
+    public class Permissions : IPermissionProvider
+    {
+        public static readonly Permission ViewOrders = new Permission { Description = "View Orders", Name = "ViewOrders" };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions()
+        {
+            return new[] {
+                ViewOrders
+            };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes()
+        {
+            return new[] {
+                new PermissionStereotype {
+                    Name = "Administrator",
+                    Permissions = new[] {ViewOrders}
+                },
+                new PermissionStereotype {
+                    Name = "Authenticated",
+                    Permissions = new[] {ViewOrders}
+                }
+            };
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.OrderLog/Routes.cs
@@ -0,0 +1,42 @@
+﻿using Orchard.Mvc.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Time.OrderLog
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 5,
+                     Route = new Route(
+                        "OrderLog/{action}/{id}",
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"},
+                                                    {"controller", "Home"},
+                                                    {"action", "Index"},
+                                                    {"id", null}
+                                                },
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"},
+                                                    {"controller", "Home"},
+                                                },
+                        new RouteValueDictionary {
+                                                    {"area", "Time.OrderLog"}
+                                                },
+                        new MvcRouteHandler())}};
+        }
+    }
+}
