2f06fd24826e46dca25c975ee99beb2eafde525d
Paul Millsaps <paulm@timemfg.com>
2014-07-25 07:16:50 -0500

    Added Time.Epicor Interim tables functionality

---
+++ src/Orchard.Web/Modules/Time.Epicor/Permissions.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+using System.Collections.Generic;
+
+namespace Time.Epicor
+{
+    public class Permissions : IPermissionProvider
+    {
+        public static readonly Permission EpicorAccess = new Permission { Description = "Epicor Access", Name = "EpicorAccess" };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions()
+        {
+            return new[] {
+                EpicorAccess
+            };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes()
+        {
+            return new[] {
+                new PermissionStereotype {
+                    Name = "Administrator",
+                    Permissions = new[] {EpicorAccess}
+                },
+                new PermissionStereotype {
+                    Name = "Authenticated",
+                    Permissions = new[] {EpicorAccess}
+                }
+            };
+        }
+    }
+}
