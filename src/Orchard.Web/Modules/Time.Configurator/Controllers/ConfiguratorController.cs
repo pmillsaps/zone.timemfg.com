c45f6466ca7e0003d5b8cbd6f7a81f6e0501561f
Paul Millsaps <paulm@timemfg.com>
2015-07-06 08:09:50 -0500

    Added Configurator module

---
+++ src/Orchard.Web/Modules/Time.Configurator/Controllers/ConfiguratorController.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Time.Configurator.Controllers
+{
+    [Themed]
+    public class ConfiguratorController : Controller
+    {
+        // GET: Configurator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
