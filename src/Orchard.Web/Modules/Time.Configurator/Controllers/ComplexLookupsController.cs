0160184cfbbf0fa20e459063241ec48fb52fe7f6
Jacob Kolar <JacobK@timemfg.com>
2015-07-16 13:16:31 -0500

    Updating the ComplexLookupsController

--- src/Orchard.Web/Modules/Time.Configurator/Controllers/ComplexLookupsController.cs
+++ src/Orchard.Web/Modules/Time.Configurator/Controllers/ComplexLookupsController.cs
@@ -37,7 +37,7 @@ namespace Time.Configurator.Controllers
         // GET: ComplexLookups
         public ActionResult Index()
         {
-            return View(db.ComplexLookups.ToList());
+            return View(db.ComplexLookups.OrderBy(x => x.ConfigName).ThenBy(x => x.ConfigData).ToList());
         }
 
         // GET: ComplexLookups/Details/5
@@ -58,6 +58,7 @@ namespace Time.Configurator.Controllers
         // GET: ComplexLookups/Create
         public ActionResult Create()
         {
+            GenerateDropDowns();
             return View();
         }
 
@@ -66,15 +67,20 @@ namespace Time.Configurator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,ConfigName,ConfigData,LookupData")] ComplexLookup complexLookup)
+        public ActionResult Create([Bind(Exclude = "Id")] ComplexLookup complexLookup)
         {
+            var Configs = db.ComplexLookups.FirstOrDefault(x => x.ConfigName == complexLookup.ConfigName && x.ConfigData == complexLookup.ConfigData && x.LookupData == complexLookup.LookupData
+            && x.Id != complexLookup.Id);
+
+            if (Configs != null) ModelState.AddModelError("", "Duplicate Option Created---Please Recheck Inputed Data");
+
             if (ModelState.IsValid)
             {
                 db.ComplexLookups.Add(complexLookup);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
+            GenerateDropDowns(complexLookup);
             return View(complexLookup);
         }
 
@@ -90,6 +96,7 @@ namespace Time.Configurator.Controllers
             {
                 return HttpNotFound();
             }
+            GenerateDropDowns();
             return View(complexLookup);
         }
 
@@ -98,14 +105,20 @@ namespace Time.Configurator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,ConfigName,ConfigData,LookupData")] ComplexLookup complexLookup)
+        public ActionResult Edit(ComplexLookup complexLookup)
         {
+            var Configs = db.ComplexLookups.FirstOrDefault(x => x.ConfigName == complexLookup.ConfigName && x.ConfigData == complexLookup.ConfigData && x.LookupData == complexLookup.LookupData
+            && x.Id != complexLookup.Id);
+
+            if (Configs != null) ModelState.AddModelError("", "Duplicate Option Created---Please Recheck Inputed Data");
+
             if (ModelState.IsValid)
             {
                 db.Entry(complexLookup).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            GenerateDropDowns(complexLookup);
             return View(complexLookup);
         }
 
@@ -142,6 +155,37 @@ namespace Time.Configurator.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private void GenerateDropDowns()
+        {
+            //prevent duplicates from showing up in drop down
+            //without var list codes, every CFG and Global shows up in drop down and whatever else for the other drop downs
+            var ConfigNameList = from firstList in db.ComplexLookups
+                                 group firstList by firstList.ConfigName into newList1
+                                 let x = newList1.FirstOrDefault()
+                                 select x;
+
+            var ConfigDataList = from secondList in db.ComplexLookups
+                                 group secondList by secondList.ConfigData into newList2
+                                 let x = newList2.FirstOrDefault()
+                                 select x;
+
+            var LookupDataList = from thirdList in db.ComplexLookups
+                           group thirdList by thirdList.LookupData into newList3
+                           let x = newList3.FirstOrDefault()
+                           select x;
+
+            ViewBag.ConfigName = new SelectList(ConfigNameList.ToList(), "ConfigName", "ConfigName");
+            ViewBag.ConfigData = new SelectList(ConfigDataList.ToList(), "ConfigData", "ConfigData");
+            ViewBag.LookupData = new SelectList(LookupDataList.ToList(), "LookupData", "LookupData");
+        }
+
+        private void GenerateDropDowns(ComplexLookup complexLookup)
+        {
+            ViewBag.ConfigName = new SelectList(db.ComplexLookups.OrderBy(x => x.ConfigName), "ConfigName", "ConfigName", complexLookup.ConfigName);
+            ViewBag.ConfigData = new SelectList(db.ComplexLookups.OrderBy(x => x.ConfigData), "ConfigData", "ConfigData", complexLookup.ConfigData);
+            ViewBag.LookupData = new SelectList(db.ComplexLookups.OrderBy(x => x.LookupData), "LookupData", "LookupData", complexLookup.LookupData);
         }
     }
 }
