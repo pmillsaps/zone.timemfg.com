303cb7bfd7115bfa1f2ec50f762bbc93d62ec9b0
Juan Salas <JuanS@timemfg.com>
2015-12-08 09:15:27 -0600

    Added more line space to Data text box in the ImporSeq view of the Structure controller.
    Added message to the NowReport view: "Page still under construction"

--- src/Orchard.Web/Modules/Time.Epicor/Controllers/NOWReportController.cs
+++ src/Orchard.Web/Modules/Time.Epicor/Controllers/NOWReportController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Orchard;
+using Orchard.Localization;
+using Orchard.Themes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +9,18 @@ using System.Web.Mvc;
 
 namespace Time.Epicor.Controllers
 {
+    [Themed]
     public class NOWReportController : Controller
     {
+        public IOrchardServices Services { get; set; }
+        public Localizer T { get; set; }
+
+        public NOWReportController(IOrchardServices services)
+        {
+            Services = services;
+            T = NullLocalizer.Instance;
+        }
+
         // GET: NOWReport
         public ActionResult Index()
         {
