f728f1b8a0c85c9dca0e5bd2a7dba074a20c43ea
Paul Millsaps <paulm@timemfg.com>
2015-12-11 15:51:23 -0600

    Added Time.DataPlates module

---
+++ src/Orchard.Web/Modules/Time.DataPlates/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Time.DataPlates.Controllers
+{
+    [Themed]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
