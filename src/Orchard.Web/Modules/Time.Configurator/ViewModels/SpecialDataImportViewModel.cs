5750c152fe462c2172c3ca37023a6cf9f7d01e21
Juan Salas <JuanS@timemfg.com>
2016-04-20 17:05:05 -0500

    Modified the RedirecToAction in the AddAdditionalDrawing action

---
+++ src/Orchard.Web/Modules/Time.Configurator/ViewModels/SpecialDataImportViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Time.Configurator.ViewModels
+{
+    public class SpecialDataImportViewModel
+    {
+        [Required(ErrorMessage = "Select a Special Config Name")]
+        public int SpecialConfigId { get; set; }
+        [Required(ErrorMessage = "Import Data Required")]
+        public string ImportData { get; set; }
+    }
+}
