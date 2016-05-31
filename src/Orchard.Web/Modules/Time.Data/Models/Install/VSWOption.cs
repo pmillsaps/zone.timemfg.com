d1b92bdc947e9a7d3deecbe09cad54ff2d7ed031
Juan Salas <JuanS@timemfg.com>
2016-05-31 11:29:03 -0500

    Added Time Options controller. Added validation to the Install module

---
+++ src/Orchard.Web/Modules/Time.Data/Models/Install/VSWOption.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Time.Data.EntityModels.Install
+{
+    [MetadataType(typeof(VSWOptionMetadata))]
+    public partial class VSWOption
+    {
+    }
+
+    public class VSWOptionMetadata
+    {
+        [Display(Name = "Lift Family Name")]
+        [Required(ErrorMessage = "Lift Family is Required")]
+        public int LiftFamilyId { get; set; }
+
+        [Display(Name = "Group Name")]
+        [Required(ErrorMessage = "Group Name is Required")]
+        public int GroupId { get; set; }
+
+        [Display(Name = "Option Name")]
+        [Required(ErrorMessage = "Option Name is Required")]
+        public string OptionName { get; set; }
+
+        [Required(ErrorMessage = "Quantity is Required")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Price is Required")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Install Hours")]
+        [Required(ErrorMessage = "Hours are Required")]
+        public decimal LaborHours { get; set; }
+
+        [Display(Name = "Paint Rate?")]
+        public bool PaintFlag { get; set; }
+    }
+}
--- src/Orchard.Web/Modules/Time.Install/ViewModels/AddOptionsViewModel.cs
+++ src/Orchard.Web/Modules/Time.Install/ViewModels/AddOptionsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,16 @@ namespace Time.Install.ViewModels
     // This model aids with adding VSW options to the db 
     public class AddOptionsViewModel
     {
+        [Display(Name = "Lift Family Name")]
+        [Required(ErrorMessage = "Group Name is required")]
         public int GroupId { get; set; }
+
+        [Display(Name = "Group Name")]
+        [Required(ErrorMessage = "Lift Family Name is required")]
         public int LiftFamilyId { get; set; }
+
+        [Display(Name = "Options to Import")]
+        [Required(ErrorMessage = "Options are required")]
         public string OptionsToParse { get; set; }
     }
 }
