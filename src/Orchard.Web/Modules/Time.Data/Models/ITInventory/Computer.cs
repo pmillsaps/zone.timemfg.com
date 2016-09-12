5d99852d69d1a46951e36b64fa7cf354c72e6990
Paul Millsaps <paulm@timemfg.com>
2016-09-12 09:36:23 -0500

    Metatata and column changes in IT Inventory sheet

--- src/Orchard.Web/Modules/Time.Data/Models/ITInventory/Computer.cs
+++ src/Orchard.Web/Modules/Time.Data/Models/ITInventory/Computer.cs
@@ -16,11 +16,24 @@ namespace Time.Data.EntityModels.ITInventory
     {
         [Required]
         public string Name { get; set; }
+
         [DataType(DataType.MultilineText)]
         public string Note { get; set; }
+
         [DataType(DataType.MultilineText)]
         public string AdditionalHW { get; set; }
+
         [DataType(DataType.PhoneNumber)]
+        [DisplayName("Phone")]
         public string PhoneNumber { get; set; }
+
+        [DisplayName("Purch Date")]
+        public DateTime PurchaseDate { get; set; }
+
+        [DisplayName("Build Date")]
+        public DateTime LastBuildDate { get; set; }
+
+        [DisplayName("Edit Date")]
+        public DateTime LastDateEdited { get; set; }
     }
 }
