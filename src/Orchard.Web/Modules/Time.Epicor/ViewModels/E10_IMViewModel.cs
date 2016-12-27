b40e1f6ce88d72ed2954ae5967dc4aaa5dc88db1
Juan Salas <JuanS@timemfg.com>
2016-12-27 16:46:09 -0600

    Added the Checkbox picture to the Load List page

---
+++ src/Orchard.Web/Modules/Time.Epicor/ViewModels/E10_IMViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Time.Data.EntityModels.Production;
+
+namespace Time.Epicor.ViewModels
+{
+    public class E10_IMViewModel
+    {
+        public List<IMPartBin> ImPartBin { get; set; }
+        public List<IMJobOper> ImJobOper { get; set; }
+    }
+}
