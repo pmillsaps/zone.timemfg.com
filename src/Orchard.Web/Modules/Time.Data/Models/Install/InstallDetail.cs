e8975f15e0188ac0da52102c06b183552e661f81
Jacob Kolar <JacobK@timemfg.com>
2017-01-12 08:09:00 -0600

    add missing nuget packages

---
+++ src/Orchard.Web/Modules/Time.Data/Models/Install/InstallDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Time.Data.EntityModels.Install
+{
+    [MetadataType(typeof(InstallDetailMetadata))]
+    public partial class InstallDetail
+    {
+    }
+
+    public class InstallDetailMetadata
+    {
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public Nullable<decimal> ExtendedPrice { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.OrderLog/Models/OrderDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time.OrderLog.Models
+{
+    public class OrderDetails
+    {
+        public string PONum { get; set; }
+        public string OrderDate { get; set; }
+        public string LiftModel { get; set; }
+        public string DealerName { get; set; }
+        public string InstallType { get; set; }
+        public string InstallerName { get; set; }
+        public int OrderQty { get; set; }
+        public int? Price { get; set; }
+        public int? ExtPrice { get; set; }
+        public bool Special { get; set; }
+        public bool Stock { get; set; }
+        public bool Demo { get; set; }
+        public bool RTG { get; set; }
+        public bool TruGuard { get; set; }
+        public string Customer { get; set; }
+        public string CityStateZip { get; set; }
+        public List<OrderTransactions> OrderT { get; set; }
+    }
+}
