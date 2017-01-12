7b4da5f0ef1fb4ec8e6e0aff0246aea05675d552
Juan Salas <JuanS@timemfg.com>
2017-01-12 09:56:55 -0600

    Added up and down arrow to the Monitor Table header

---
+++ src/Orchard.Web/Modules/Time.Configurator/Models/SpecialConfigViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time.Configurator.Models
+{
+    public class SpecialConfigViewModel
+    {
+        public int SpecialConfigId { get; set; }
+        public int SpecialDataTypeId { get; set; }
+        public string Part { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int? RelatedOpId { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.Data/Models/ITInventory/Ref_Building.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Time.Data.EntityModels.ITInventory
+{
+    [MetadataType(typeof(Ref_BuildingMetadata))]
+    public partial class Ref_Building
+    {
+    }
+
+    public class Ref_BuildingMetadata
+    {
+        [DisplayName("Building")]
+        public string Name { get; set; }
+    }
+}
