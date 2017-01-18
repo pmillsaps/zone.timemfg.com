7970a43838d352fb77f70f5ce9a73aa31d0c9647
Juan Salas <JuanS@timemfg.com>
2017-01-18 08:10:38 -0600

    Completed task for Ticket# 12682. Added requested fields. Alse added date picker to Create and Edit in the computer controller.

--- src/Orchard.Web/Modules/Time.Data/Models/ITInventory/Computer.cs
+++ src/Orchard.Web/Modules/Time.Data/Models/ITInventory/Computer.cs
@@ -15,12 +15,14 @@ namespace Time.Data.EntityModels.ITInventory
     public class ComputerMetadata
     {
         [Required]
+        [DisplayName("Computer Name")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
         public string Note { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [DisplayName("Additional HW")]
         public string AdditionalHW { get; set; }
 
         [DataType(DataType.PhoneNumber)]
@@ -35,5 +37,41 @@ namespace Time.Data.EntityModels.ITInventory
 
         [DisplayName("Edit Date")]
         public DateTime LastDateEdited { get; set; }
+
+        [DisplayName("Status")]
+        public int StatusId { get; set; }
+
+        [DisplayName("Model")]
+        public int ModelId { get; set; }
+
+        [DisplayName("Windows Key")]
+        public string WindowsKey { get; set; }
+
+        [DisplayName("Memory")]
+        public Nullable<int> MemoryId { get; set; }
+
+        [DisplayName("Processor")]
+        public Nullable<int> ProcessorId { get; set; }
+
+        [DisplayName("Device Type")]
+        public Nullable<int> DeviceTypeId { get; set; }
+
+        [DisplayName("OS")]
+        public Nullable<int> OSId { get; set; }
+
+        [DisplayName("Video Card")]
+        public Nullable<int> VideoCardId { get; set; }
+
+        [DisplayName("Sound")]
+        public Nullable<int> SoundId { get; set; }
+
+        [DisplayName("Last Edited By")]
+        public string LastEditedBy { get; set; }
+
+        [DisplayName("WTY Expiration Date")]
+        public Nullable<System.DateTime> WarrantyExpirationDate { get; set; }
+
+        [DisplayName("Serial Number")]
+        public string SerialNumber { get; set; }
     }
 }
