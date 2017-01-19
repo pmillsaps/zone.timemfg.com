0710b801c7c4a5dc12dff514c939f15dfba6f1a9
Juan Salas <JuanS@timemfg.com>
2017-01-19 12:47:24 -0600

    Added dynamic table to the Users page

---
+++ src/Orchard.Web/Modules/Time.Epicor/ViewModels/DistributorOrderListVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Time.Data.EntityModels.Production;
+
+namespace Time.Epicor.ViewModels
+{
+    public class DistributorOrderListVM
+    {
+        public int Distributor { get; set; }
+        public IEnumerable<V_DistributorOrderList> Orders { get; set; }
+    }
+}
