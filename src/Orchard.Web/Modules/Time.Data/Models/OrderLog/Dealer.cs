a8b3f1accf9f4ebd73d0eed42edad3525f97b82a
Jacob Kolar <JacobK@timemfg.com>
2017-01-30 10:57:08 -0600

    added resource complete checkbox to tickets

---
+++ src/Orchard.Web/Modules/Time.Data/Models/OrderLog/Dealer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Time.Data.EntityModels.OrderLog
+{
+    [MetadataType(typeof(DealerMetadata))]
+    public partial class Dealer
+    {
+    }
+
+    public class DealerMetadata
+    {
+        [DisplayName("Dealer")]
+        [Required]
+        public string DealerName { get; set; }
+        [DisplayName("Territory")]
+        public int TerritoryId { get; set; }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.IT/Models/MonitorsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time.IT.Models
+{
+    public class MonitorsViewModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string MFRName { get; set; }
+        public string Model { get; set; }
+        public string SerialNo { get; set; }
+        public string AssetId { get; set; }
+        public string Size { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public string PurchasedFrom { get; set; }
+        public string PO { get; set; }
+        public decimal? Cost { get; set; }
+        public string Notes { get; set; }
+    }
+}
