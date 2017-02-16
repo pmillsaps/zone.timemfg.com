71ba48736289286ae9678d1cf9eddbc7eb9fb190
Jacob Kolar <JacobK@timemfg.com>
2017-02-16 14:51:50 -0600

    moved ticket resource changed notification

---
+++ src/Orchard.Web/Modules/Time.Epicor/Models/ValuedInventoryExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Time.Epicor.Models
+{
+    public class ValuedInventoryExt
+    {
+        public int Id { get; set; }
+        public string PartNum { get; set; }
+        public string Description { get; set; }
+        public decimal BOH { get; set; }
+        public System.DateTime CalculationDate { get; set; }
+        public System.DateTime TransactionDate { get; set; }
+        public decimal TransactionCost { get; set; }
+        public decimal CurrentCost { get; set; }
+        public System.DateTime ComparisonDate { get; set; }
+        public int PeriodYear { get; set; }
+        public string ClassId { get; set; }
+
+        public decimal ExtCost
+        {
+            get { return this.BOH * this.CurrentCost; }
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.Install/Business Logic/InsertIntoInstallDetailsAndManuallyAddedOptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Time.Data.EntityModels.Install;
+using Time.Install.ViewModels;
+
+namespace Time.Install.Business_Logic
+{
+    public class InsertIntoInstallDetailsAndManuallyAddedOptions
+    {
+        // Saving the install quote details to the db
+        public static void InsertData(QuoteViewModel vm, VSWQuotesEntities dbQ)
+        {
+            var installQid = dbQ.InstallQuotes.Where(x => x.LiftQuoteNumber == vm.QuoteNum).Select(x => new { installId = x.Id }).Single();
+            foreach (var item in vm.GroupAndOptions.Options.Where(x => x.Quantity > 0))// VSW Options in the db
+            {
+                InstallDetail installD = new InstallDetail
+                {
+                    InstallQuoteId = installQid.installId,
+                    GroupId = item.GroupId,
+                    OptionId = item.Id,
+                    Quantity = item.Quantity,
+                    Price = item.Price,
+                    InstallHours = (item.InstallHours * item.Quantity)
+                };
+                dbQ.InstallDetails.Add(installD);
+            }
+            foreach (var item in vm.AddOptnMnlly.Where(x => x.AddQuantityManually > 0))// VSW Options added manually
+            {
+                InstallDetailsManuallyAddedOption installDM = new InstallDetailsManuallyAddedOption
+                {
+                    InstallQuoteId = installQid.installId,
+                    OptionName = item.AddOptionManually,
+                    Quantity = item.AddQuantityManually,
+                    Price = item.AddPriceManually,
+                    InstallHours = (item.AddInstallHoursManually * item.AddQuantityManually),
+                    PaintFlag = item.AddPaintFlagManually
+                };
+                dbQ.InstallDetailsManuallyAddedOptions.Add(installDM);
+            }
+            dbQ.SaveChanges();
+        }
+    }
+}
