853cbb9ebf971bd706ffe84fb428857f0650e002
Juan Salas <JuanS@timemfg.com>
2017-03-27 13:51:37 -0500

    Fixed and issue with the Users page not displaying all users

---
+++ src/Orchard.Web/Modules/Time.Install/Controllers/InstallDetailsForWordDocsController.cs
@@ -0,0 +1,141 @@
+﻿using Orchard;
+using Orchard.Localization;
+using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Time.Data.EntityModels.Install;
+
+namespace Time.Install.Controllers
+{
+    [Themed]
+    [Authorize]
+    public class InstallDetailsForWordDocsController : Controller
+    {
+        private VSWQuotesEntities db = new VSWQuotesEntities();
+
+        private string ErrorMessage { get; set; }
+        public IOrchardServices Services { get; set; }
+        public Localizer T { get; set; }
+
+        public InstallDetailsForWordDocsController(IOrchardServices services)
+        {
+            Services = services;
+        }
+
+        // GET: InstallDetailsForWordDocs
+        public ActionResult Index()
+        {
+            return View(db.InstallDetailsForWordDocs.ToList());
+        }
+
+        // GET: InstallDetailsForWordDocs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallDetailsForWordDoc installDetailsForWordDoc = db.InstallDetailsForWordDocs.Find(id);
+            if (installDetailsForWordDoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installDetailsForWordDoc);
+        }
+
+        // GET: InstallDetailsForWordDocs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InstallDetailsForWordDocs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "Id")] InstallDetailsForWordDoc installDetailsForWordDoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InstallDetailsForWordDocs.Add(installDetailsForWordDoc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(installDetailsForWordDoc);
+        }
+
+        // GET: InstallDetailsForWordDocs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallDetailsForWordDoc installDetailsForWordDoc = db.InstallDetailsForWordDocs.Find(id);
+            if (installDetailsForWordDoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installDetailsForWordDoc);
+        }
+
+        // POST: InstallDetailsForWordDocs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(InstallDetailsForWordDoc installDetailsForWordDoc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(installDetailsForWordDoc).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(installDetailsForWordDoc);
+        }
+
+        // GET: InstallDetailsForWordDocs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallDetailsForWordDoc installDetailsForWordDoc = db.InstallDetailsForWordDocs.Find(id);
+            if (installDetailsForWordDoc == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installDetailsForWordDoc);
+        }
+
+        // POST: InstallDetailsForWordDocs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InstallDetailsForWordDoc installDetailsForWordDoc = db.InstallDetailsForWordDocs.Find(id);
+            db.InstallDetailsForWordDocs.Remove(installDetailsForWordDoc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/Time.Legacy/Models/InsertWarranty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Time.Data.EntityModels.Legacy;
+
+namespace Time.Legacy.Models
+{
+    public class InsertWarranty
+    {
+        //public WarrantyInformation Information { get; set; }
+        //public WarrantyInvoice Invoice { get; set; }
+
+        public int Id { get; set; }
+        public string SerialNumber { get; set; }
+        public string LiftModel { get; set; }
+        public string EndUserName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string LiftOrderNumber { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string PoNumber { get; set; }
+        public string Comments { get; set; }
+    }
+}
